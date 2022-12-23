1a43eafda8d5cb4781ff3715ec593a2f83a2a25a
LilDrikks <87108295+LilDrikks@users.noreply.github.com>
2022-12-23 10:28:02 -0300

    url vercel

--- src/requests/axios.js
+++ src/requests/axios.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 
+const url = 'https://deploy-node-lildrikks.vercel.app'
+
+
 export const signInPut = async (email, password) => {
   const body = { email: email, password: password }
   try {
-    const res = await axios.put(`http://localhost:4000/user`, body)
+    const res = await axios.put(`${url}/user`, body)
     return res.data
   } catch (err) {
     return err.response.data
@@ -13,7 +16,7 @@ export const signInPut = async (email, password) => {
 export const signUpPost = async (email, password) => {
   const body = { email: email, password: password, approved: false }
   try {
-    const res = await axios.post('http://localhost:4000/user', body)
+    const res = await axios.post(`${url}/user`, body)
     return res.data
   } catch (err) {
     return err.response.data.err
