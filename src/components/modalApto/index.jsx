8ceca18e8d632f05d18e8781fc99be69e75787d9
LilDrikks <87108295+LilDrikks@users.noreply.github.com>
2023-01-04 03:05:51 -0300

    lista de moradores modal

--- src/components/modalApto/index.jsx
+++ src/components/modalApto/index.jsx
@@ -1,32 +1,61 @@
-import React, { useState } from 'react'
-import * as C from './styles'
-import * as D from '../ItensSideBar/styles'
-import {x} from 'react-icons-kit/feather/x'
-import Icon from 'react-icons-kit'
-import useAuth from '../../hooks/useAuth'
+import React, { useState } from "react";
+import * as C from "./styles";
+import * as D from "../ItensSideBar/styles";
+import { x } from "react-icons-kit/feather/x";
+import { pencil } from "react-icons-kit/icomoon/pencil";
+import Icon from "react-icons-kit";
+import useAuth from "../../hooks/useAuth";
+import Input from "../Input";
 
 function Modal() {
-  
-  const { setModal, moradores } = useAuth()
-  console.log(moradores[2][1])
+  const { setModal, moradores, setMoradores } = useAuth();
+  const [edit, setEdit] = useState({ state: false, id: "", nome: "" });
+  console.log(edit);
   return (
     <C.ModalApto>
-      <C.Moradores>
+      <C.ModalContainer>
         <C.Apto>
-          <span>
-          {moradores[0]}
-          </span>
-          <span>
-          {moradores[1]}
-          </span>
+          <span>{moradores[0]}</span>
+          <span>{moradores[1]}</span>
         </C.Apto>
-
-        <D.Item style={{width:'40px', height: '40px'}} onClick={() => setModal(false)} >
-          <Icon icon={x} size={20} style={{color:'black'}} />
+        <C.Moradores>
+          {moradores[2].map((item, index) => (
+            <li key={index}>
+              <p>{item.nome}</p>
+              <span>
+                <D.Item
+                  style={{ width: "26px", height: "26px" }}
+                  onClick={(e) =>
+                    setEdit({ state: true, id: item._id, nome: item.nome })
+                  }
+                >
+                  <Icon icon={pencil} size={12} style={{ color: "black" }} />
+                </D.Item>
+              </span>
+            </li>
+          ))}
+          <D.Item style={{ width: "40px", height: "40px" }} className={'button'}>
+            <Icon icon={x} size={20} style={{ color: "black" }} className={'plus'}/>
+            <p>adicionar morador</p>
+          </D.Item>
+        </C.Moradores>
+        {edit.state ? (
+          <Input
+            value={edit.nome}
+            onChange={(e) => setEdit({ state: true, nome: e.target.value })}
+          />
+        ) : (
+          false
+        )}
+        <D.Item
+          style={{ width: "40px", height: "40px" }}
+          onClick={() => setModal(false)}
+        >
+          <Icon icon={x} size={20} style={{ color: "black" }} />
         </D.Item>
-      </C.Moradores>
+      </C.ModalContainer>
     </C.ModalApto>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
--- src/components/modalApto/styles.jsx
+++ src/components/modalApto/styles.jsx
@@ -11,7 +11,7 @@ export const ModalApto = styled.div`
   align-items: center;
 `;
 
-export const Moradores = styled.div`
+export const ModalContainer = styled.div`
   width: 400px;
   height: 400px;
   border-radius: 5px;
@@ -23,6 +23,75 @@ export const Moradores = styled.div`
     top: 10px;
   }
 `;
+export const Moradores = styled.ul`
+  margin: 20px;
+  list-style: none;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border: 1px solid;
+  height: 100%;
+  max-height: 300px;
+  button{
+    position:static;
+    align-self:center;
+  }
+  .button{
+    p {
+    visibility: hidden;
+    width: 120px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 5px 5px;
+    position: absolute;
+    z-index: 1;
+    bottom: 0;
+    left: 0;
+    margin-left: -60px;
+    opacity: 0;
+    transition: opacity 0.3s;
+    font-size: 14px;
+    @media (max-width: 500px) {
+      bottom: 110%;
+      left: 50%;
+    }
+  }
+
+  p::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #555 transparent transparent transparent;
+    @media (max-width: 500px) {
+      top: 100%;
+      left: 50%;
+    }
+  }
+
+  &:hover p {
+    visibility: visible;
+    opacity: 1;
+  }
+  }
+
+  li{
+    border-bottom: 1px solid;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    button{
+      position:static;
+    }
+  }
+`
 export const Apto = styled.h3`
   display: flex;
   justify-content: center;
